59c71219eb551bb717df8f7d0aaa3c98b98bc788
yvette <yvette-werner@web.de>
2025-12-21 12:59:53 +0100

    fixed appli_scaling()

--- src/scaling.py
+++ src/scaling.py
@@ -9,10 +9,10 @@ def get_scaler(name, params={}):
     cls = SCALER_REGISTRY[name]
     return cls(**params)
 
-def apply_scaling(X_train, X_test, scaler_list):
-
-    for scaler_name in scaler_list:
-     scaler = scaler_name()
+def apply_scaling(X_train, X_test, config):
+    scaler_list = config["preprocessing"]["scaling"]
+    for item in scaler_list:
+     scaler = get_scaler(item["scaler"], item["params"])
      X_train = scaler.fit_transform(X_train)
      X_test = scaler.transform(X_test)
 

317cd59813b047586ed1c0d0054827e8394358a9
yvette <yvette-werner@web.de>
2026-01-04 14:00:47 +0100

    add inverse transform for evaluation

--- src/scaling.py
+++ src/scaling.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 SCALER_REGISTRY = {
@@ -9,7 +10,7 @@ def get_scaler(name, params={}):
     cls = SCALER_REGISTRY[name]
     return cls(**params)
 
-def apply_scaling(X_train, X_test, scaler_list):
+def apply_scaling(X_train, scaler_list):
 
     for item in scaler_list:
         scaler = get_scaler(item["scaler"], item["params"])
@@ -26,5 +27,41 @@ class LogScaler:
         return np.log2(X+1)
     def fit_transform(self, X):
         return self.fit(X).transform(X)
+    def inverse_transform(self, X):
+        return 2**(X)-1
 
 SCALER_REGISTRY["log2"] = LogScaler
+
+class ScalerWrapper():
+    def __init__(self, scaler_names):
+        self.scaler_list = []
+        for name in scaler_names:
+            self.scaler_list.append(get_scaler(name))
+    
+    def fit(self, X):
+        X_ = X
+        for scaler in self.scaler_list:
+            scaler.fit(X_)
+            X_ = scaler.transform(X_)
+        return self
+
+    def transform(self, X):
+        columns = X.columns
+        index = X.index
+        X_ = X.copy()
+        for scaler in self.scaler_list:
+            X_ = scaler.transform(X_)
+        df = pd.DataFrame(X_, index=index, columns=columns)
+        return df
+    
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
+    
+    def inverse_transform(self, X):
+        columns = X.columns
+        index = X.index
+        X_ = X.copy()
+        for scaler in reversed(self.scaler_list):
+            X_ = scaler.inverse_transform(X_)
+        df = pd.DataFrame(X_, index=index, columns=columns)
+        return df

ccd1c9ce429e174c6e4ce7e9d9fa07329c76a8bf
yvette <yvette-werner@web.de>
2026-01-07 13:00:17 +0100

    add feature agglomeration for feature selection

--- src/model_registry.py
+++ src/model_registry.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import ElasticNet, Lars, LassoLars
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, AdaBoostRegressor
 from sklearn.svm import SVR
 from sklearn.decomposition import PCA
+from sklearn.cluster import FeatureAgglomeration
 from sklearn.base import BaseEstimator, RegressorMixin, clone
 from pytorch_tabular import TabularModel
 from pytorch_tabular.models import TabTransformerConfig
@@ -61,3 +62,16 @@ class ClassifierGuidedRegressor(BaseEstimator, RegressorMixin):
         return (y > 0).astype(int)
 
 MODEL_REGISTRY["hierarchical"] = ClassifierGuidedRegressor
+
+class CustomFeatureAgg(FeatureAgglomeration):
+    def __init__(self, n_clusters = 2, *, metric = "euclidean", memory = None, connectivity = None, compute_full_tree = "auto", linkage = "ward", pooling_func = np.mean, distance_threshold = None, compute_distances = False):
+        super().__init__(n_clusters, metric=metric, memory=memory, connectivity=connectivity, compute_full_tree=compute_full_tree, linkage=linkage, pooling_func=pooling_func, distance_threshold=distance_threshold, compute_distances=compute_distances)
+
+    def fit(self, X, y=None):
+        N = X.shape[1]
+        conn = np.tri(N)
+        conn[np.diag_indices(N)]=0
+        self.connectivity=conn
+        return super().fit(X,y)
+
+MODEL_REGISTRY["feature_agg"] = CustomFeatureAgg

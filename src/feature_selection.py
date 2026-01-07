247f620ba2420c98ffa4f6d3a8302f909db702c0
yvette <yvette-werner@web.de>
2026-01-07 12:59:43 +0100

    add class that can act as feature selector based on scikit-learn transformation

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -1,4 +1,5 @@
 from sklearn.feature_selection import SelectFromModel, VarianceThreshold
+from sklearn.base  import BaseEstimator, TransformerMixin
 
 
 FEATURE_SELECTION_REGISTRY = {
@@ -12,3 +13,18 @@ def get_feature_selector(name, estimator, params=None):
     if params==None:
         params = {}
     return FEATURE_SELECTION_REGISTRY[name](estimator, **params)
+
+class SelectFromTansformation(BaseEstimator, TransformerMixin):
+    def __init__(self, estimator):
+        super().__init__()
+        self.estimator = estimator
+    def fit(self, X, y=None):
+        return self.estimator.fit(X)
+    def fit_transform(self, X, y = None):
+        return self.estimator.fit_transform(X, y)
+    def transform(self, X, y=None):
+        return self.estimator.transform(X)
+    def inverse_transform(self, X, y=None):
+        return self.estimator.inverse_transform(X)
+
+FEATURE_SELECTION_REGISTRY["scikitlearn_transformation"]=SelectFromTansformation
